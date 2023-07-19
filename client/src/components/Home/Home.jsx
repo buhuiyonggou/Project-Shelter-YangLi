9673ec2da18e78dfeb21852eb6f5532781388556
buhuiyonggou <yangli4944@gmail.com>
2023-07-19 13:40:24 -0700

    update format

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,50 +1,51 @@
 import './home.css';
-
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
+import VideoCarousel from '../VideoCarousel/VideoCarousel';
 
 function Home() {
   const { user, isAuthenticated } = useAuth0();
-  const [videoId, setVideoId] = useState('nrOIhOsbSDk');  
+  const [videoIds, setVideoIds] = useState([]);  
 
   useEffect(() => {
-    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC7JBG8BXxqTgu6jpeJikImQ&q=${videoId}&key=AIzaSyBvB8S03KRxNVscvlNMLMlMrpM7THvVpyc`)
+    fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UC7JBG8BXxqTgu6jpeJikImQ&maxResults=5&key=AIzaSyBvB8S03KRxNVscvlNMLMlMrpM7THvVpyc`)
     .then((result) => {
         return result.json(); 
     })
     .then((data) => {
-        if (data.items.length > 0) {
-          setVideoId(data.items[0].id.videoId); 
-        }
+        const ids = data.items.map(item => item.id.videoId);
+        setVideoIds(ids);
     })
     .catch((error) => {
         console.log(error);
     });
-  }, [videoId]);
+  }, []);
 
   return (
     <div className={'home-container'}>
       <h1 className={'text-center bg-orange text-white'}>Welcome to Puppy Adoption Center!</h1>
       <div className={'row flex jc-c'}>
-        <div className={'paper col-6 col-s-10 bg-image'}>
+        <div className={'wallpaper col-6 col-s-10'}>
           <div className={'introduction'}>
             <h1>The North Eastern Shelter</h1>
             <div className='contact-info'>
               <p>Phone: 204 123 4567</p> 
               <p>Address: 410 W Georgia St #1400, Vancouver</p>
             </div>
-            {!isAuthenticated ? (<div>(<Link to="/dashboard">
-              <button className={'button button-blue'}>
-                Login
-              </button></Link>)
+            {!isAuthenticated ? (<div>
+              <Link to="/dashboard">
+                <button className={'button button-blue'}>
+                  Login
+                </button>
+              </Link> 
               <strong className={'instruction'}>
               <i>
                 to start the adoption process.
               </i>
               </strong>
-              </div>) : (
-            <div className='goProfile'>
+            </div>) : 
+              (<div className='goProfile'>
               {user && (
                 <div>  
                   <p className='user-greeting'>Welcome, {user.name}!</p>
@@ -60,13 +61,8 @@ function Home() {
             </div>
           </div>
           <div className="video-container">
-          <iframe title="api" 
-            src={`https://www.youtube.com/embed/${videoId}?loop=1&modestbranding=1`}
-            width={"80%"}
-            height={"320"}
-            frameBorder={0}
-            allowFullScreen>
-          </iframe>
+              <p>See our videos here: </p>
+            <VideoCarousel videoIds={videoIds} />
           </div>
         </div>
       </div>
--- client/src/components/VideoCarousel/VideoCarousel.jsx
+++ client/src/components/VideoCarousel/VideoCarousel.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import './videoCarousel.css';
 
 function VideoCarousel({ videoIds }) {
     return (
@@ -8,12 +9,13 @@ function VideoCarousel({ videoIds }) {
             showArrows={true}
             infiniteLoop={true}
             showThumbs={false}
-            autoPlay={false}>
+            autoPlay={false}
+            className="carousel-container">
             {videoIds.map(videoId => (
-                <div key={videoId}>
+                <div key={videoId} className="video-wrapper">
                     <iframe
-                        width="100%"
-                        height="100%"
+                        width="200"
+                        height="270"
                         src={`https://www.youtube.com/embed/${videoId}`}
                         title={`video-${videoId}`}
                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
