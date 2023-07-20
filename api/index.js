c400982a20c6c697bd0afcf3d389d91529beeb02
buhuiyonggou <yangli4944@gmail.com>
2023-07-19 17:34:11 -0700

    update deployment

--- api/index.js
+++ api/index.js
@@ -504,8 +504,9 @@ app.post("/verify-user", requireAuth, async (req, res) => {
   res.json(client);
 });
 
-const PORT = 8000;
+const PORT = parseInt(process.env.PORT) || 8080;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT} 🎉 🚀`);
- });
+ console.log(`Server running on http://localhost:${PORT} 🎉 🚀`);
+});
+
  
